c487362b82e2927d59630c97a7509a1ab128e2ff
Joao Luiz de Figueiredo Magalhães <dukejlf@gmail.com>
2022-12-13 21:09:29 -0300

    Finished the test.

--- _assets/js/onerhino.js
+++ _assets/js/onerhino.js
@@ -1,8 +1,35 @@
 (function($) {
 
   $('#items_list').sortable({
+    update: function (event, ui) {
+      callAjax('reorder_list');
+      
+    }
+  }).on('click', '.delete', function() {
+    $(this).closest('li').remove(); 
 
+    callAjax('remove_item');
+    
   });
+
+
+  function callAjax(action) {
+    var siteDomain =  document.location.origin + '/jobsite/wp-admin/admin-ajax.php';
+      
+    // POST to server using $.post or $.ajax
+    $.ajax({
+        data: {
+          action: action,
+          data: $('#items_list').sortable('toArray')
+        },
+        type: 'POST',
+        url: siteDomain,
+        success: function(data){
+          console.log(data)
+          alert('success updated the list');
+        }
+    });
+  }
 
 
 })( jQuery );
--- onerhino.php
+++ onerhino.php
@@ -12,14 +12,20 @@
     if(get_option('onerhino_items')) {
 
       $items = maybe_unserialize(get_option('onerhino_items'));
-      
-      array_push($items, $_POST['new_item']);
+
+      $copy = $items;
+
+      ksort($copy);
+
+      $last_item = (int)substr(array_key_last($copy), -1);
+
+      $items['item_' . ($last_item + 1) ] = $_POST['new_item'];
 
       update_option('onerhino_items', maybe_serialize($items));
 
     } else {
 
-      add_option('onerhino_items', maybe_serialize(array($_POST['new_item'])));
+      add_option('onerhino_items', maybe_serialize(array('item_0' => $_POST['new_item'])));
 
     }
 
@@ -30,24 +36,25 @@
     ?>
     <div class="wrap">
       <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-      <form action="<?php menu_page_url( 'wporg' ) ?>" method="post">
 
-        <?php 
+      <?php 
 
           $items = maybe_unserialize(get_option('onerhino_items'));
           
           ?><ul id="items_list"><?php
           foreach($items as $key => $item) {
             ?>
-              <li id="<?= $key ?>" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><?= $item ?></li>
+              <li id="<?= $key ?>" class=" option ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><?= $item ?> <span class="ui-icon ui-icon-trash delete"></li>
             <?php
           }
           ?></ul><?php
 
         ?>
+      
+      <form action="<?php menu_page_url( 'wporg' ) ?>" method="post">
         
         <label for="new_item">Item value</label>
-        <input type="text" name="new_item">
+        <input type="text" name="new_item" required>
         
         <?php
           submit_button( __( 'Save Settings', 'textdomain' ) );
@@ -79,8 +86,81 @@
     wp_enqueue_script('jquery_ui_js');
 
     wp_enqueue_style('jquery_ui_css', plugins_url( '', __FILE__ ).'/_assets/vendor/jquery-ui/jquery-ui.min.css');
+    wp_enqueue_style('onerhino_css', plugins_url( '', __FILE__ ).'/_assets/css/onerhino.css');
 
     wp_register_script('onerhino_js', plugins_url( '', __FILE__ ).'/_assets/js/onerhino.js', array('jquery'),'1.0', true);
     wp_enqueue_script('onerhino_js');
+
+  }
+
+
+
+  //This function send email message
+  add_action( 'wp_ajax_reorder_list',  'reorder_list');
+  add_action( 'wp_ajax_nopriv_reorder_list','reorder_list');
+
+  function reorder_list() {
+
+    $order_key = $_POST['data'];
+
+    $items = maybe_unserialize(get_option('onerhino_items'));
+
+    $reorder_items = array_merge(array_flip($order_key), $items );
+
+    update_option('onerhino_items', maybe_serialize($reorder_items ));
+
+  }
+
+  //This function send email message
+  add_action( 'wp_ajax_remove_item',  'remove_item');
+  add_action( 'wp_ajax_nopriv_remove_item','remove_item');
+
+  function remove_item() {
+
+    $order_key = $_POST['data'];
+
+    $items = maybe_unserialize(get_option('onerhino_items'));
+
+    $new_list = array_intersect_key($items, array_flip($order_key) );
+
+    update_option('onerhino_items', maybe_serialize($new_list ));
+
+  }
+
+
+  add_shortcode('listofitems','listofitemsFunction');
+  function listofitemsFunction($atts){
+
+    $items = maybe_unserialize(get_option('onerhino_items'));
+
+    if($items) {
+
+      ob_start(); 
+      
+      ?><ul><?php
+
+        if( $atts['limit'] ) {
+
+          $limit = (int)$atts['limit'];
+
+          $items = array_slice($items, 0, $limit, true);
+     
+        }
+
+        foreach($items as $key => $item) {
+          ?>
+            <li id="<?= $key ?>" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><?= $item ?><span class="ui-icon ui-icon-trash delete"></li>
+          <?php
+        }
+
+      ?></ul><?php
+
+      return ob_get_clean();
+    
+    } else {
+
+      return 'Sorry, no items to show';
+
+    }
 
   }
